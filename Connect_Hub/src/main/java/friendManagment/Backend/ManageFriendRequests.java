6bf067906fe8fb157562726dc61a6dd83b0ff113
mirollawa2l <89114408+mirollawa2l@users.noreply.github.com>
2024-12-05 20:37:35 +0200

    delete unnessecary import

--- Connect_Hub/src/main/java/friendManagment/Backend/ManageFriendRequests.java
+++ Connect_Hub/src/main/java/friendManagment/Backend/ManageFriendRequests.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import userdatabasemanagement.UserDatabaseManagement;
 import userdatabasemanagement.User;
 import java.util.ArrayList;
-import 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
