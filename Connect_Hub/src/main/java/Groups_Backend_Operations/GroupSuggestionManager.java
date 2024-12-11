e8838d907ff386841f30588764a1a31d987af2a2
mirollawa2l <89114408+mirollawa2l@users.noreply.github.com>
2024-12-11 23:26:33 +0200

    Add new package and arrange files

--- Connect_Hub/src/main/java/Groups_Backend_Operations/GroupSuggestionManager.java
+++ Connect_Hub/src/main/java/Groups_Backend_Operations/GroupSuggestionManager.java
@@ -2,8 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Groups_Backend;
+package Groups_Backend_Operations;
 
+import Groups_Backend.Group;
+import Groups_Backend.GroupManager;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 import userdatabasemanagement.User;
